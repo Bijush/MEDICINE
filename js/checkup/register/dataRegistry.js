3774b9d2053cbb7fc908ae9e3bf9582680cfc435
bijush <38435589+Bijush@users.noreply.github.com>
2026-05-26 08:06:49 +0530

    Add files via upload

--- js/checkup/register/dataRegistry.js
+++ js/checkup/register/dataRegistry.js
@@ -1,1 +1,169 @@
 
+/*
+// ==============================
+// FEVER
+// ==============================
+console.log("DATA REGISTRY LOADED");
+import { DENGUE }
+from "../../../data/checkup/fever/dengue.js";
+
+import { MALARIA }
+from "../../../data/checkup/fever/malaria.js";
+
+import { TYPHOID }
+from "../../../data/checkup/fever/typhoid.js";
+/*
+import { COVID }
+from "../../../data/checkup/fever/covid.js";
+*/
+import { INFLUENZA }
+from "../../../data/checkup/fever/influenza.js";
+
+import { TUBERCULOSIS }
+from "../../../data/checkup/fever/tuberculosis.js";
+
+
+import { VIRAL_FEVER }
+from "../../../data/checkup/fever/viralFever.js";
+/*
+// ==============================
+// LIVER
+// ==============================
+
+import { HEPATITIS } from "../../../data/checkup/liver/hepatitis.js";
+
+import { FATTY_LIVER } from "../../../data/checkup/liver/fattyLiver.js";
+
+import { CIRRHOSIS } from "../../../data/checkup/liver/cirrhosis.js";
+
+import { JAUNDICE } from "../../../data/checkup/liver/jaundice.js";
+
+
+
+import { LIVER_FAILURE } from "../../../data/checkup/liver/liverFailure.js";
+
+import { LIVER_CANCER } from "../../../data/checkup/liver/liverCancer.js";
+
+// NEW
+import { LIVER_FIBROSIS } from "../../../data/checkup/liver/fibrosis.js";
+import { ALCOHOLIC_LIVER } from "../../../data/checkup/liver/alcoholicLiver.js";
+import { CHOLESTASIS } from "../../../data/checkup/liver/cholestasis.js";
+import { LIVER_ABSCESS } from "../../../data/checkup/liver/liverAbscess.js";
+import { PORTAL_HYPERTENSION } from "../../../data/checkup/liver/portalHypertension.js";
+import { HEPATIC_ENCEPHALOPATHY } from "../../../data/checkup/liver/hepaticEncephalopathy.js";
+
+// ==============================
+// RESPIRATORY
+// ==============================
+
+import { ASTHMA } from "../../../data/checkup/respiratory/asthma.js";
+
+import { COPD } from "../../../data/checkup/respiratory/copd.js";
+
+import { PNEUMONIA } from "../../../data/checkup/respiratory/pneumonia.js";
+import { BRONCHITIS } from "../../../data/checkup/respiratory/bronchitis.js";
+import { SINUSITIS } from "../../../data/checkup/respiratory/sinusitis.js";
+import { COMMON_COLD } from "../../../data/checkup/respiratory/commonCold.js";
+
+
+// ==============================
+// STOMACH
+// ==============================
+
+
+
+import { FOOD_POISONING } from "../../../data/checkup/stomach/foodPoisoning.js";
+
+import { GERD } from "../../../data/checkup/stomach/gerd.js";
+
+import { PEPTIC_ULCER } from "../../../data/checkup/stomach/pepticUlcer.js";
+
+import { GASTROENTERITIS } from "../../../data/checkup/stomach/gastroenteritis.js";
+
+import { APPENDICITIS } from "../../../data/checkup/stomach/appendicitis.js";
+
+import { PANCREATITIS } from "../../../data/checkup/stomach/pancreatitis.js";
+
+import { IBS } from "../../../data/checkup/stomach/ibs.js";
+
+import { ULCERATIVE_COLITIS } from "../../../data/checkup/stomach/ulcerativeColitis.js";
+
+import { CROHNS_DISEASE } from "../../../data/checkup/stomach/crohnsDisease.js";
+
+import { CONSTIPATION } from "../../../data/checkup/stomach/constipation.js";
+
+import { HEMORRHOIDS }
+from "../../../data/checkup/stomach/hemorrhoids.js";
+
+import { INTESTINAL_OBSTRUCTION }
+from "../../../data/checkup/stomach/intestinalObstruction.js";
+
+import { GASTRIC_CANCER }
+from "../../../data/checkup/stomach/gastricCancer.js";
+// heart
+import { HEART_ATTACK }
+from "../../../data/checkup/heart/heartAttack.js";
+*/
+// ==============================
+// ALL DISEASES
+// ==============================
+
+export const ALL_DISEASES = [
+
+  // fever
+/*
+  ...DENGUE,
+  ...MALARIA,
+  ...TYPHOID,
+  */
+  /*
+  ...COVID,
+  */
+  ...INFLUENZA,
+  ...TUBERCULOSIS,
+  
+  ...VIRAL_FEVER
+/*
+// liver
+
+...HEPATITIS,
+...FATTY_LIVER,
+...CIRRHOSIS,
+...JAUNDICE,
+...LIVER_FAILURE,
+...LIVER_CANCER,
+...LIVER_FIBROSIS,
+...ALCOHOLIC_LIVER,
+...CHOLESTASIS,
+...LIVER_ABSCESS,
+...PORTAL_HYPERTENSION,
+...HEPATIC_ENCEPHALOPATHY,
+
+// respiratory
+
+...ASTHMA,
+...COPD,
+...PNEUMONIA,
+...BRONCHITIS,
+...SINUSITIS,
+...COMMON_COLD,
+
+// stomach
+
+...FOOD_POISONING,
+...GERD,
+...PEPTIC_ULCER,
+...GASTROENTERITIS,
+...APPENDICITIS,
+...PANCREATITIS,
+...IBS,
+...ULCERATIVE_COLITIS,
+...CROHNS_DISEASE,
+...CONSTIPATION,
+...HEMORRHOIDS,
+...INTESTINAL_OBSTRUCTION,
+...GASTRIC_CANCER,
+// heart
+...HEART_ATTACK
+*/
+];
